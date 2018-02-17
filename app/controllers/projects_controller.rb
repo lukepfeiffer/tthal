f7fc629233b46217e5e260dbf235bfd3d7d0c074
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2018-02-17 16:50:39 -0500

    Many style additions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,23 @@ class ProjectsController < ApplicationController
   expose :projects do
     Project.all
   end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(
+      :title,
+      :subtitle,
+      :details,
+      :body
+    )
+  end
 end
